6368707d8e85dfda1c625c305be80bdec529b916
maksvolokh22@gmail.com <maksvolokh22@gmail.com>
2023-02-23 14:07:42 +0200

    Create project FootballManager

---
+++ FootballManagerAPI/FootballManagerAPI/Controllers/FootballManagerController.cs
@@ -0,0 +1,28 @@
+﻿using FootballManagerAPI.Controllers.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FootballManagerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FootballManagerController : ControllerBase
+    {
+        [HttpGet]
+
+        public async Task<ActionResult<List<FootballPlayer>>> Get()
+        {
+            var players = new List<FootballPlayer>();
+            {
+                new FootballPlayer(1, "Maksym", "Volokh", 18)
+                {
+
+                };
+            }
+            return Ok(players);
+        }
+
+    }
+}
---
+++ FootballManagerAPI/FootballManagerAPI/Controllers/Models/FootballPlayer.cs
@@ -0,0 +1,18 @@
+﻿namespace FootballManagerAPI.Controllers.Models
+{
+    public class FootballPlayer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Number { get; set; }
+
+        public FootballPlayer(int id, string firstName, string lastName, int number)
+        {
+            this.Id = id;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Number = number;
+        }
+    }
+}
